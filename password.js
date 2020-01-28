119ea35bf2e61b1594d72ae718c3bcce24ddd429
K-ai30 <artpirate11@aim.com>
2020-01-28 00:52:58 -0500

    fixed clipboard button

--- password.js
+++ password.js
@@ -6,6 +6,7 @@ var symbolElement = document.querySelector("#symbols");
 var length = document.querySelector("#length");
 var generateButton = document.querySelector("#generate");
 var result = document.querySelector("#result");
+var clipboardButton = document.querySelector("#clipboard");
 
 // Global variables
 var isUpperCase = true;
@@ -18,43 +19,90 @@ let alphabet = "abcdefghijklmnopqrstuvyxz";
 let numbers = "0123456789";
 let symbols = "~`!@#$%^&*()_-+=?";
 
+let userSelection = 4;
+
 // Generate the password
 generateButton.addEventListener('click', function(event) {
     console.log('Value: ', length.value);
-    generateUserPassword();
+    var passwordlength = length.value;
+    // Short password alert, stops password from generating
+    if (passwordlength < 8){
+        alert('Password is too short!');
+    } else {
+        generateUserPassword(passwordlength);
+    }
 });
 
+// This code checks to see if the user selected or deselected Uppercase letters to include them in password
 upperCaseElement.addEventListener('change', function() {
     isUpperCase = upperCaseElement.checked;  // reset
+    if (isUpperCase === false) {
+        userSelection--;
+    } else {
+        userSelection++;
+    }
+    console.log("User Selection: ", userSelection);
 });
 
+// This code checks to see if the user selected or deselected Lowercase letters to include them in password
 lowerCaseElement.addEventListener('change', function() {
     isLowerCase = lowerCaseElement.checked;
+    if (isLowerCase === false) {
+        userSelection--;
+    } else {
+        userSelection++;
+    }
+    console.log("User Selection: ", userSelection);
 });
 
+// This code checks to see if the user selected or deselected Numbers to include them in password
 numberElement.addEventListener('change', function() {
     isNumbers = numberElement.checked;
+    if (isNumbers === false) {
+        userSelection--;
+    } else {
+        userSelection++;
+    }
+    console.log("User Selection: ", userSelection);
 });
 
+// This code checks to see if the user selected or deselected Symbols to include them in password
 symbolElement.addEventListener('change', function() {
     isSymbols = symbolElement.checked;
+    if (isSymbols === false) {
+        userSelection--;
+    } else {
+        userSelection++;
+    }
+    console.log("User Selection: ", userSelection);
 });
 
-let userLength = 8;  // Min Value hard coded
-
+// This function grabs the password from the text area that the user inputs to save to the clipboard
 function copyToClipboard() {
     var textArea = document.querySelector("#result");
     textArea.select();
     document.execCommand('copy');
 }
 
-function generateUserPassword() {
-    var password = '';
+// This code calls the function to save password
+clipboardButton.addEventListener('click', function() {
+    copyToClipboard();
+});
 
-    for (var i = 0; i < userLength; i++) {
+// This function generates my password with the "parameters" based on what the user needs
+function generateUserPassword(passwordlength) {
+    var password = '';
+    // This code indicates that the application should only select random letters, numbers, or symbols when buttons are selected/deselected
+    for (var i = 0; i < passwordlength - userSelection; i++) {
         var randomLetterNumber = Math.floor(Math.random() * alphabet.length); // 0
         var letter = alphabet[randomLetterNumber];
-        password += letter;
+        // Lowercase letters
+        if (isLowerCase === true) {
+            password += letter;
+        } else {
+            // Uppercase letters
+            password += letter.toUpperCase();
+        }
     }
 
     if (isUpperCase === true) {
@@ -66,14 +114,16 @@ function generateUserPassword() {
     if (isNumbers === true) {
         var randomNumber = Math.floor(Math.random() * numbers.length); // 0
         var num = numbers[randomNumber];
+        console.log('Number: ', randomNumber);
         password += num;
     }
 
     if (isSymbols === true) {
         var randomSymbolNumber = Math.floor(Math.random() * symbols.length); // 0
         var symbol = symbols[randomSymbolNumber];
-        password += symbol + num + letter;
+        password += symbol;
         console.log(password);
+        // Tells the code to print password
         result.innerHTML = password;
     }
 };
